8b1a8ebfd082015931cba98a16715f17fbfc4bac
3Davydov <davydovdaniil501@gmail.com>
2024-05-15 11:27:05 +0700

    MANAG-19: delete cors processing

--- internal/app/app.go
+++ internal/app/app.go
@@ -29,16 +29,16 @@ func NewApp(log *slog.Logger, cfg *config.Config, envVars *env.Env) (*App, error
 	const op = "app.NewApp"
 
 	router := gin.Default()
-	router.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusOK)
-			return
-		}
-		c.Next()
-	})
+	// router.Use(func(c *gin.Context) {
+	// 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	// 	if c.Request.Method == "OPTIONS" {
+	// 		c.AbortWithStatus(http.StatusOK)
+	// 		return
+	// 	}
+	// 	c.Next()
+	// })
 
 	newRegService, err := regImpl.NewService(cfg.GrpcAuthServer.Address)
 	if err != nil {
